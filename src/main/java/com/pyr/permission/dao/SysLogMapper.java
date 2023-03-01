09e9ff07e612e56fd91bab5d81c25c537b095850
panyuro <panyuro@163.com>
2023-03-01 22:25:39 +0800

    提交使用MyBatis Generator 自动生成的代码

---
+++ src/main/java/com/pyr/permission/dao/SysLogMapper.java
@@ -0,0 +1,17 @@
+package com.pyr.permission.dao;
+
+import com.pyr.permission.model.SysLog;
+
+public interface SysLogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysLog record);
+
+    int insertSelective(SysLog record);
+
+    SysLog selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysLog record);
+
+    int updateByPrimaryKey(SysLog record);
+}
---
+++ src/main/java/com/pyr/permission/dao/SysRoleUserMapper.java
@@ -0,0 +1,17 @@
+package com.pyr.permission.dao;
+
+import com.pyr.permission.model.SysRoleUser;
+
+public interface SysRoleUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRoleUser record);
+
+    int insertSelective(SysRoleUser record);
+
+    SysRoleUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysRoleUser record);
+
+    int updateByPrimaryKey(SysRoleUser record);
+}
