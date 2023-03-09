aa70f2e44dd12508b45b191499369c393b550159
panyuro <panyuro@163.com>
2023-03-09 13:41:51 +0800

    测试全局异常处理

---
+++ src/main/java/com/pyr/permission/controller/UserRestController.java
@@ -0,0 +1,52 @@
+package com.pyr.permission.controller;
+
+import com.pyr.permission.exception.BizException;
+import com.pyr.permission.model.SysUser;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class UserRestController {
+
+ @PostMapping("/user")
+    public boolean insert(@RequestBody SysUser user) {
+     System.out.println("开始新增...");
+     //如果姓名为空就手动抛出一个自定义的异常！
+        if(user.getUserName()==null){
+            throw  new BizException("-1","用户姓名不能为空！");
+        }
+        return true;
+    }
+
+    @PutMapping("/user")
+    public boolean update(@RequestBody SysUser user) {
+     System.out.println("开始更新...");
+       //这里故意造成一个空指针的异常，并且不进行处理
+        String str=null;
+        str.equals("111");
+        return true;
+    }
+
+    @DeleteMapping("/user")
+    public boolean delete(@RequestBody SysUser user)  {
+        System.out.println("开始删除...");
+        //这里故意造成一个异常，并且不进行处理
+        Integer.parseInt("abc123");
+        return true;
+    }
+
+    @GetMapping("/user")
+    public List<SysUser> findByUser(SysUser user) {
+     System.out.println("开始查询...");
+        List<SysUser> userList =new ArrayList<>();
+        SysUser user2=new SysUser();
+        user2.setId(1);
+        user2.setUserName("xuwujing");
+        userList.add(user2);
+        return userList;
+    }
+    
+}
--- src/main/java/com/pyr/permission/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pyr/permission/exception/GlobalExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.pyr.permission.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
