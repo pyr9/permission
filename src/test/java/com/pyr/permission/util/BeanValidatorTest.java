67d742aa115c3f3e3e77d9132ee534baa84a1ea8
panyuro <panyuro@163.com>
2023-03-12 14:52:26 +0800

    校验工具BeanValidator开发
    - Hibernate Validator 参考： https://www.jianshu.com/p/0bfe2318814f

---
+++ src/test/java/com/pyr/permission/util/BeanValidatorTest.java
@@ -0,0 +1,42 @@
+package com.pyr.permission.util;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.val;
+import org.hibernate.validator.constraints.NotBlank;
+import org.junit.Test;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class BeanValidatorTest {
+
+    @Test
+    public void validateObject() {
+        val user = new User();
+        val validateResult = BeanValidator.validateObject(user);
+        validateResult.forEach((k, v) -> {
+            System.out.print(k + "=" + v + " ");
+        });
+    }
+
+    @Test
+    public void check() {
+        val user = new User();
+        BeanValidator.check(user);
+    }
+
+
+    @Getter
+    @Setter
+    class User {
+        @NotBlank
+        private String msg;
+
+        @NotNull
+        @Max(value = 10, message = "id不能大于10")
+        @Min(value = 10, message = "id不能小于1")
+        private Integer id;
+    }
+}
