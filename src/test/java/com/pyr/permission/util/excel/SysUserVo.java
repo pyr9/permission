bc3cfc80314d453131f7c4b32cee10cfed5cb735
panyuro <panyuro@163.com>
2023-08-10 23:04:58 +0800

    test 测试beanutils实现copy对象

--- src/test/java/com/pyr/permission/util/excel/SysUserVo.java
+++ src/test/java/com/pyr/permission/util/excel/SysUserVo.java
@@ -3,6 +3,15 @@ package com.pyr.permission.util.excel;
 import com.pyr.permission.common.excel.I18nText;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.converters.DateConverter;
+import org.apache.commons.beanutils.converters.DateTimeConverter;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Setter
 @Getter
@@ -21,4 +30,22 @@ public class SysUserVo {
     private Integer departmentId;
 
     private Integer status;
+
+    private Date birthday;
+
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        DateTimeConverter dateConverter = new DateConverter(null);
+        dateConverter.setPatterns(new String[]{"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"});
+        ConvertUtils.register(dateConverter, Date.class);
+
+        Map<String, String> dataMap = new HashMap<>();
+        dataMap.put("userName", "John Smith");
+        dataMap.put("birthday", "1990-01-01T00:00:00.000Z");
+
+        SysUserVo user = new SysUserVo();
+        BeanUtils.populate(user, dataMap);
+        System.out.println(user.getUserName());
+        System.out.println(user.getBirthday());
+    }
 }
